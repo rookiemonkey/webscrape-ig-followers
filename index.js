ccb3ce0471d56f62427731222e251905287452ed
rookiemonkey <kevinroirigorbasina@pm.me>
2022-07-22 15:00:49 +0800

    :wrench: tested with 100K data

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 require('dotenv').config()
+const fs = require("fs");
 const puppeteer = require('puppeteer');
+const { parse } = require("csv-parse");
+const getStream = require('get-stream');
+
+const added_headers = 'ig_followers,email,remarks'
+const targetFileName = 'shopgram-report-first-10K.csv';
+const outputPath = `./output/${targetFileName}`;
+const inputPath = `./src/${targetFileName}`
+const noop = () => { }
+
+async function parseCSV(filePath) {
+  const parseStream = parse({ delimiter: ',' });
+  const data = await getStream.array(fs.createReadStream(filePath).pipe(parseStream));
+  return data;
+}
 
 (async function(){
 
   try {
+    console.time(`Measure IG-Webscraper for ${targetFileName}`)
+
     // initialize the browser, set headless to false to see how it works
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
@@ -18,25 +35,92 @@ const puppeteer = require('puppeteer');
     await page.type('[name=username]', process.env.ig_username);
     await page.type('[name=password]', process.env.ig_password);
     await page.click('[type=submit]');
-
-    // wait for full page load
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-    // redirect to target instagram account and wait until page has been loaded
-    const targetInstagramAccount = 'https://www.instagram.com/mileycyrus'
-    await page.goto(targetInstagramAccount, { waitUntil: 'networkidle0'});
+    // recreate existing output
+    if (fs.existsSync(targetFileName)) await fs.unlinkSync(targetFileName)
+    await fs.writeFileSync(outputPath, '')
 
-    // extract the followers count by dom manipulation
-    const followersCount = await page.evaluate(() => {
-      const el = [...document.querySelectorAll('div')].filter(el => (el.innerHTML.includes('followers') && el.innerText.length <= 35))[0]
-      return parseInt(el.querySelector('span').title.replaceAll(',', ''))
-    })
+    // begin parsing
+    const rows = await parseCSV(inputPath)
 
-    console.log(`${targetInstagramAccount} has ${followersCount}`)
+    for(let row = 0; row < rows.length; row++) {
+      const data = rows[row]
+      const outputRow = [...data]
+      const categories = data[6]
+      const igUrl = data[7]
+      const emails = data[14].split(' ')
+
+      let appendTimes = 0
+      let ig_followers = 'NO IG URL'
+      let remarks = 'NO MATCHING CATEGORY'
+
+      // HEADERS: removes the exess commas at the end
+      if(row === 0) {
+        data.splice(15, outputRow.length)
+        await fs.appendFile(outputPath, `${data.join(",")},${added_headers}\n`, noop)
+        continue;
+      }
+
+      // CLEAN DATA: normalize everything and enclose inside "" if there is a data
+      for (let i in outputRow) outputRow[i] ? outputRow[i] = `"${outputRow[i]}"` : null
+
+      // CLEAN DATA: converting the raw excel file to csv generated bunch of commas at the end after the emails (index 14) column, removed it
+      outputRow.splice(15, outputRow.length)
+
+      // APPEND MULTIPLE TIMES: determine nth times to append on file
+      if ((emails.length-1) > 0) appendTimes = (emails.length-1)
+
+
+      /**
+       * IG FOLLOWERS
+       */
+      // DETERMINE IG FOLLOWERS COUNT: if there is an IG URL
+      if (igUrl) {
+        //  redirect to target instagram account and wait until page has been loaded
+        await page.goto(igUrl, { waitUntil: 'networkidle0' });
+
+        // extract the followers count by dom manipulation
+        ig_followers = await page.evaluate(() => {
+          const el = [...document.querySelectorAll('div')].filter(el => (el.innerHTML.includes('followers') && el.innerText.length <= 35))[0]
+          return parseInt(el.querySelector('span').title.replaceAll(',', ''))
+        })
+      }
+
+      // apppend the data, added an empty string for the email will be determined before appending
+      outputRow.push(ig_followers, '')
+
+
+
+      /**
+       * REMARKS
+       */
+      let hasShirts = categories.includes('Shirts') || categories.includes('Shirt');
+      let hasHoodies = categories.includes('Hoodies') || categories.includes('Hoodie');
+
+      if (hasShirts) remarks = 'Shirts';
+      if (hasHoodies) remarks = 'Hoodies';
+      if (hasShirts && hasHoodies) remarks = 'Shirts, Hoodies';
+
+      outputRow.push(remarks)
+
+
+
+      /**
+       * EMAILS
+       */
+
+      for (let count = 0; count <= appendTimes; count++) {
+        outputRow[16] = emails[count]
+        await fs.appendFile(outputPath, `${outputRow.join(",")}\n`, noop)
+      }
+    }
+
+    console.timeEnd(`Measure IG-Webscraper for ${targetFileName}`)
   }
 
   catch(e) {
-    console.log('ERROR: ', e)
+    console.log('[ERROR]: ', e)
   }
 
 })()
